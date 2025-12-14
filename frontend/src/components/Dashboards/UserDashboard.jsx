73a9446dd3c9ccbebda744fbeaa1a6774df88ce8
JamesMungai254 <mungaijames6303@gmail.com>
2025-12-14 09:42:02 +0300

    link update

--- frontend/src/components/Dashboards/UserDashboard.jsx
+++ frontend/src/components/Dashboards/UserDashboard.jsx
@@ -92,7 +92,7 @@ function UserDashboard() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" to="/">Home</a>
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#contact">Contact</a>
