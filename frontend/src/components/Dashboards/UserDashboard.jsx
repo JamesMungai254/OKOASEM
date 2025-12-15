a6173e98f7dc294151f36b1fba4e8e46590c83ff
JamesMungai254 <mungaijames6303@gmail.com>
2025-12-15 09:20:23 +0300

    nav upload

--- frontend/src/components/Dashboards/UserDashboard.jsx
+++ frontend/src/components/Dashboards/UserDashboard.jsx
@@ -86,7 +86,7 @@ function UserDashboard() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">OKOASEM</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" bg="white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
