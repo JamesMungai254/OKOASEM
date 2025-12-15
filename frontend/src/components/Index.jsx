0b8bb5d4b3d66a2aedd59d99e744962ecc227a5f
JamesMungai254 <mungaijames6303@gmail.com>
2025-12-15 10:01:10 +0300

    css update

--- frontend/src/components/Index.jsx
+++ frontend/src/components/Index.jsx
@@ -13,8 +13,8 @@ function Index() {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark " style={{opacity:'0.9'}}>
         <div className="container-fluid">
     <a className="navbar-brand" href="#">OKOASEM</a>
-    <button className="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" style={{backgroundColor:'white'}}>X</span>
+    <button className="navbar-toggler bg-white"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" ></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
