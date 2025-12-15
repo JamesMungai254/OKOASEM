1ed243f849be4718132652a29c6770fbc0ef9ad4
JamesMungai254 <mungaijames6303@gmail.com>
2025-12-15 10:11:46 +0300

    css update

--- frontend/src/components/Dashboards/UserDashboard.jsx
+++ frontend/src/components/Dashboards/UserDashboard.jsx
@@ -87,7 +87,7 @@ function UserDashboard() {
         <div className="container-fluid">
           <a className="navbar-brand" href="/">OKOASEM</a>
           <button className="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon bg-blue"></span>
+              <span className="navbar-toggler-icon bg-white" style={{color:'black'}} >☰</span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
