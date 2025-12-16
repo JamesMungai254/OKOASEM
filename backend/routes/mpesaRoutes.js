573341e9d23ca71098a0ab7f3eb9ffb859ba49d7
JamesMungai254 <mungaijames6303@gmail.com>
2025-12-17 02:45:24 +0300

    mpesa integration

--- backend/routes/mpesaRoutes.js
+++ backend/routes/mpesaRoutes.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const router = express.Router();
+const cors = require('cors');
+
+// Enable CORS for this router
+router.use(cors({
+  origin: 'https://okoasemfrontend.onrender.com',
+  methods: ['GET','POST','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
+
 const {
   initiatePayment,
   mpesaCallback
 } = require("../controllers/mpesaController");
 
-const router = express.Router();
+
 
 router.post("/pay", initiatePayment);
 router.post("/mpesa/callback", mpesaCallback);
